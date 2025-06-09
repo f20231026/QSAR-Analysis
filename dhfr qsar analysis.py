50de213754a5f7cfdc059fe643a16677dcfc1c9e
Mahathi <152363766+f20231026@users.noreply.github.com>
2025-06-09 23:09:55 +0530

    Add files via upload

---
+++ dhfr qsar analysis.py
@@ -0,0 +1,75 @@
+
+from matplotlib import pyplot as plt 
+
+
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+
+
+
+
+
+# Convert SMILES to molecule
+inhibitors = [('Methotrexate','CN(CC1=CN=C2C(=N1)C(=NC(=N2)N)N)C3=CC=C(C=C3)C(=O)N[C@@H](CCC(=O)O)C(=O)O')
+,('Trimethoprim','COC1=CC(=CC(=C1OC)OC)CC2=CN=C(N=C2N)N'),
+('Aminopterin', 'C1=CC(=CC=C1C(=O)N[C@@H](CCC(=O)O)C(=O)O)NCC2=CN=C3C(=N2)C(=NC(=N3)N)N'),
+
+('Pralatrexate', 'CCCC(CC1=CN=C2C(=N1)C(=NC(=N2)N)N)C3=CC=C(C=C3)C(=O)N[C@@H](CCC(=O)O)C(=O)O'),
+
+('Metoprine' ,'CC1=C(C(=NC(=N1)N)N)C2=CC(=C(C=C2)Cl)Cl'),
+('WR99210', 'CC1(N=C(N=C(N1OCCCOC2=CC(=C(C=C2Cl)Cl)Cl)N)N)C')
+        
+         
+
+]
+mahi=[]
+i=0         
+for name,smiles in inhibitors: 
+  mol = Chem.MolFromSmiles(smiles)
+  if mol :
+    logp=Descriptors.MolLogP(mol) 
+    
+    mahi.append(logp)
+    
+  i=i+1 
+
+print(mahi)
+
+sri=[]
+i=0         
+for name,smiles in inhibitors: 
+  mol = Chem.MolFromSmiles(smiles)
+  if mol :
+    tpsa=Descriptors.TPSA(mol) 
+    
+    sri.append(tpsa)
+  i=i+1    
+print(sri)
+    
+    
+
+x1=mahi
+x2=sri
+y=[-1.34,-1.732,-0.875,-0.857,-2,1.124]
+plt.scatter(x1,y,label='logp vs logic50')
+plt.scatter(x2,y,label='tpsa vs loic50 ')
+plt.title('logp vs ic50')
+plt.ylabel('ic50')
+plt.xlabel('logp')
+plt.legend()
+plt.show()
+
+from scipy.stats import linregress
+slope, intercept, r_value, p_value, std_err = linregress(x1,y)
+print(f"R²: {r_value**2}")
+
+
+from scipy.stats import linregress
+slope, intercept, r_value, p_value, std_err = linregress(x2,y)
+print(f"R²: {r_value**2}")
+
+
+
+
+
+
---
+++ floxin drug qsar.py
@@ -0,0 +1,68 @@
+from matplotlib import pyplot as plt 
+
+
+from rdkit import Chem
+from rdkit.Chem import Descriptors
+
+
+
+# Convert SMILES to molecule
+inhibitors = [('norfloxacin', 'CCN1C=C(C(=O)C2=CC(=C(C=C21)N3CCNCC3)F)C(=O)O'),
+('ciprofloxacin', 'C1CC1N2C=C(C(=O)C3=CC(=C(C=C32)N4CCNCC4)F)C(=O)O'),
+('gatifloxacin', 'CC1CN(CCN1)C2=C(C=C3C(=C2OC)N(C=C(C3=O)C(=O)O)C4CC4)F'),
+('moxifloxacin', 'COC1=C2C(=CC(=C1N3C[C@@H]4CCCN[C@@H]4C3)F)C(=O)C(=CN2C5CC5)C(=O)O')
+        
+         
+
+]
+mahi=[]
+i=0         
+for name,smiles in inhibitors: 
+  mol = Chem.MolFromSmiles(smiles)
+  if mol :
+    logp=Descriptors.MolLogP(mol) 
+    
+    mahi.append(logp)
+    
+  i=i+1 
+
+print(mahi)
+
+sri=[]
+i=0         
+for name,smiles in inhibitors: 
+  mol = Chem.MolFromSmiles(smiles)
+  if mol :
+    tpsa=Descriptors.TPSA(mol) 
+    
+    sri.append(tpsa)
+  i=i+1    
+print(sri)
+    
+    
+
+x1=mahi
+x2=sri
+y=[2.28,1.1,0.109,1]
+plt.scatter(x1,y,label='logp vs logic50')
+plt.scatter(x2,y,label='tpsa vs loic50 ')
+plt.title('logp vs ic50')
+plt.ylabel('ic50')
+plt.xlabel('logp')
+plt.legend()
+plt.show()
+
+from scipy.stats import linregress
+slope, intercept, r_value, p_value, std_err = linregress(x1,y)
+print(f"R²: {r_value**2}")
+
+
+from scipy.stats import linregress
+slope, intercept, r_value, p_value, std_err = linregress(x2,y)
+print(f"R²: {r_value**2}")
+
+
+
+
+
+
